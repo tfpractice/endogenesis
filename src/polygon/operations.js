d21adb85f292acb12b8c21f271d07b27028e66e6
theory friction practice <jackson.brebnor@gmail.com>
2017-06-07 19:44:30 -0400

    successful partitioninG

--- src/polygon/operations.js
+++ src/polygon/operations.js
@@ -28,7 +28,7 @@ export const nthVertex = poly => (n = 0) =>
 
 export const vertices = poly =>
   range(numSides(poly)).map(nthVertex(poly));
-  
+
 export const pointCount = poly => segments(poly) * numSides(poly);
 export const lerpFactor = poly => segments(poly) ? segments(poly) ** -1 : 0;
 
@@ -42,7 +42,7 @@ export const segMod = poly => i => i ? i % segments(poly) : i;
 
 export const edgePoint = poly => (i) => {
   console.log('i', i);
-
+  
   // console.log('segMod(poly)(i)', segMod(poly)(i));
   // console.log(' lerpFactor(poly)', lerpFactor(poly));
   // 
@@ -50,41 +50,34 @@ export const edgePoint = poly => (i) => {
   // 
   // console.log('nthVertex(i))', nthVertex(poly)(baseIdx(poly)(i)));
   // console.log('nthVertex(i) + 1)', nthVertex(poly)(baseIdx(poly)(i) + 1)); 
-
+  
   // console.log('(segments(poly) % i) * lerpFactor(poly))', (segments(poly) % i) * lerpFactor(poly));
   // 
   // const res = Vector.lerp(
   //   nthVertex(poly)(baseIdx(poly)(i)),
   //   nthVertex(poly)(baseIdx(poly)(i) + 1),
   //   (segMod(poly)(i) * lerpFactor(poly))); 
-  // 
+  console.log('(segments(poly) % i)', (segments(poly) % i));
+  console.log('(segments(poly) % i)', (i % segments(poly)));
+
+  console.log('segMod(poly', segMod(poly)(i));
+
   // console.log('res', res);
   return Vector.lerp(
     nthVertex(poly)(baseIdx(poly)(i)),
     nthVertex(poly)(baseIdx(poly)(i) + 1),
-    ((segments(poly) % i) * lerpFactor(poly))); 
+    ((segMod(poly)(i)) * lerpFactor(poly))); 
 };
-    
-export const edgePoints = poly =>
 
-// segments(poly) ?
- range(pointCount(poly)).map(edgePoint(poly)); 
+export const edgePoints = poly => range(pointCount(poly)).map(edgePoint(poly)); 
+ 
+export const apexFloor = poly => (pointCount(poly) - 1) % pointCount(poly);
+export const apexCeil = poly => (pointCount(poly) + 1) % pointCount(poly);
+export const apex = poly => new Vector(vertices(poly)[0].x,vertices(poly)[0].y,);
 
- // : [];
-
-// ayer.prototype.establishPartitionPoints = function() {
-//     for (var i = 0; i < this.polygon.sideCount; i++) {
-//         // console.log("display is calling the base object");
-//         // console.log(this.polygon);
-//         var nextIndex = (i + 1) % this.polygon.sideCount;
-//         var tempCenter = this.polygon.center.copy();
-//         var tempVertex = this.polygon.vertices[i].copy();
-//         var tempNext = this.polygon.vertices[nextIndex].copy();
-//         // this.polygon.sideCount[i]
-//         for (var j = 0; j < this.pointsPerEdge; j++) {
-//             var pointIndex = (this.pointsPerEdge * i) + j;
-//             var instanceLerp = j * this.lerpFactor;
-//             this.partitionPoints[pointIndex] = new p5.Vector.lerp(tempVertex, tempNext, instanceLerp);
-//         };
-//     };
+// Displayer.prototype.setApexPoints = function() {
+//    this.apexFloorIndex = (this.pointCount - 1) % this.pointCount;
+//    this.apexCeilingIndex = (this.pointCount + 1) % this.pointCount;
+//    this.base = new p5.Vector(this.polygon.center.x, this.polygon.center.y);
+//    this.apex = new p5.Vector(this.polygon.vertices[0].x, this.polygon.vertices[0].y);
 // };
