52b19254ea43f0e709ff9a557d8614757352de99
theory friction practice <jackson.brebnor@gmail.com>
2017-06-06 22:10:24 -0400

    data setting

--- __tests__/polygon/data.test.js
+++ __tests__/polygon/data.test.js
@@ -1,5 +1,7 @@
 import 'jasmine-expect';
-import { getX, getY, polygon, radius, rotation, segments, sides, } from 'src/polygon';
+import { getX, getY, numSides, polygon, radius, rotation, segments,
+  setNumSides, setRadius, setRotation, setSegments, setX, setY, } 
+from 'src/polygon';
 
 const basePoly = polygon();
 
@@ -10,8 +12,70 @@ describe('polygon', () => {
       expect(basePoly.y).toEqual(0);
       expect(basePoly.radius).toEqual(10);
       expect(basePoly.rotation).toEqual(0);
-      expect(basePoly.sides).toEqual(3);
+      expect(basePoly.numSides).toEqual(3);
       expect(basePoly.segments).toEqual(1);
+    });
+  });
+  
+  describe('getX', () => {
+    it('returns the getX proprerty', () => {
+      expect(getX(basePoly)).toEqual(0);
+    });
+  });
+  describe('getY', () => {
+    it('returns the getY proprerty', () => {
+      expect(getY(basePoly)).toEqual(0);
+    });
+  });
+  describe('radius', () => {
+    it('returns the radius proprerty', () => {
+      expect(radius(basePoly)).toEqual(10);
+    });
+  });
+  describe('rotation', () => {
+    it('returns the rotation proprerty', () => {
+      expect(rotation(basePoly)).toEqual(0);
+    });
+  });
+  describe('numSides', () => {
+    it('returns the numSides proprerty', () => {
+      expect(numSides(basePoly)).toEqual(3);
+    });
+  });
+  describe('segments', () => {
+    it('returns the segments proprerty', () => {
+      expect(segments(basePoly)).toEqual(1);
+    });
+  });
+  describe('setX', () => {
+    it('sets the setX proprerty', () => {
+      expect(setX(5)(basePoly).x).toEqual(5);
+    });
+  });
+  
+  describe('setY', () => {
+    it('sets the setY proprerty', () => {
+      expect(setY(5)(basePoly).y).toEqual(5);
+    });
+  });
+  describe('setRadius', () => {
+    it('sets the setRadius proprerty', () => {
+      expect(setRadius(5)(basePoly).radius).toEqual(5);
+    });
+  });
+  describe('setRotation', () => {
+    it('sets the setRotation proprerty', () => {
+      expect(setRotation(5)(basePoly).rotation).toEqual(5);
+    });
+  });
+  describe('setNumSides', () => {
+    it('sets the setNumSides proprerty', () => {
+      expect(setNumSides(5)(basePoly).numSides).toEqual(5);
+    });
+  });
+  describe('setSegments', () => {
+    it('sets the setSegments proprerty', () => {
+      expect(setSegments(5)(basePoly).segments).toEqual(5);
     });
   });
 });
--- src/polygon/data.js
+++ src/polygon/data.js
@@ -1,14 +1,46 @@
+const { PI, } = Math;
 const dGon = {
- x: 0, y: 0, radius: 10, rotation: 0, sides: 3, segments: 1,
+ x: 0, y: 0, radius: 10, rotation: 0, numSides: 3, segments: 1,
 };
 
-export const polygon = (x = 0, y = 0, radius = 10, rotation = 0, sides = 3, segments = 1) => ({
- x, y, radius, rotation, sides, segments,
+export const polygon = (x = 0, y = 0, radius = 10, rotation = 0, numSides = 3, segments = 1) => ({
+ x, y, radius, rotation, numSides, segments,
 }); 
 
 export const getX = ({ x, } = dGon) => x;
 export const getY = ({ y, } = dGon) => y;
 export const radius = ({ radius, } = dGon) => radius;
 export const rotation = ({ rotation, } = dGon) => rotation;
-export const sides = ({ sides, } = dGon) => sides;
+export const numSides = ({ numSides, } = dGon) => numSides;
 export const segments = ({ segments, } = dGon) => segments;
+
+export const setX = x => poly =>
+polygon(x, getY(poly), radius(poly), rotation(poly), numSides(poly), segments(poly));
+export const setY = y => poly =>
+polygon(getX(poly), y, radius(poly), rotation(poly), numSides(poly), segments(poly));
+export const setRadius = rd => poly =>
+polygon(getX(poly), getY(poly), rd, rotation(poly), numSides(poly), segments(poly));
+export const setRotation = rt => poly =>
+polygon(getX(poly), getY(poly), radius(poly), rt, numSides(poly), segments(poly));
+export const setNumSides = n => poly =>
+polygon(getX(poly), getY(poly), radius(poly), rotation(poly), n,segments(poly));
+export const setSegments = s => poly =>
+polygon(getX(poly), getY(poly), radius(poly), rotation(poly), numSides(poly), s);
+ 
+export const baseAngle = poly => 2 * PI / numSides(poly);
+export const isEven = poly => numSides(poly) % 2 === 0;
+
+// function Polygon(cx, cy, radius, rotation, numSides, dFactory) {
+//       this.cx = cx;
+//       this.cy = cy;
+//       this.radius = radius;
+//       this.rotation = rotation;
+//       this.numSides = numSides;
+//       this.dFactory = dFactory;
+//       this.pointsPerEdge = 1;
+//       this.centrality = true;
+//       this.parallel = true;
+//       this.apoFactor = Math.cos(Math.PI / this.numSides);
+//       this.update();
+//       this.setDisplayer();
+//   };
