5e5de2f093e297874c4d0b7ac6f8afc4fba713de
theory friction practice <jackson.brebnor@gmail.com>
2017-06-07 20:31:14 -0400

    polydadat

--- src/polygon/data.js
+++ src/polygon/data.js
@@ -1,4 +1,3 @@
-
 const { PI, } = Math;
 const dGon = {
   x: 0, y: 0, radius: 10, rotation: 0, numSides: 3, segments: 1,
@@ -33,33 +32,5 @@ export const setNumSides = n => poly =>
 export const setSegments = s => poly =>
   polygon(getX(poly), getY(poly), radius(poly), rotation(poly), numSides(poly), s);
 
-// export const changeX = qt => poly => setX(qt + getX(poly))(poly);
-
-// function Polygon(cx, cy, radius, rotation, numSides, dFactory) {
-//       this.cx = cx;
-//       this.cy = cy;
-//       this.radius = radius;
-//       this.rotation = rotation;
-//       this.numSides = numSides;
-//       this.dFactory = dFactory;
-//       this.pointsPerEdge = 1;
-//       this.centrality = true;
-//       this.parallel = true;
-//       this.apoFactor = Math.cos(Math.PI / this.numSides);
-//       this.update();
-//       this.setDisplayer();
-//   };
-
-// this.even = ((this.sideCount % 2) == 0) ? true : false;
-//         this.peripherals = [];
-//         this.baseAngle = ((2 * Math.PI) / this.sideCount);
-//         this.center = new p5.Vector(this.cx, this.cy);
-//         this.apoOffset = this.baseAngle / 2;
-//         this.vertices = [];
-//         this.apoMag = this.radius * Math.cos(Math.PI / this.sideCount);
-//         this.circumVector = new p5.Vector.fromAngle(this.rotation);
-//         this.circumVector.mult(this.radius);
-//         this.apoVector = new p5.Vector.fromAngle(this.rotation + this.apoOffset);
-//         this.apoVector.mult(this.apoMag);
-//         this.establishVertices();
-//         this.setDisplayer();
+      // this.centrality = true;
+      // this.parallel = true;
