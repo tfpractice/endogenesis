379c845594605a136f4c843ec8b8a86ca0cb35cb
theory friction practice <jackson.brebnor@gmail.com>
2017-06-19 15:22:04 -0400

    rotates tess

--- src/polygon/tesselate.js
+++ src/polygon/tesselate.js
@@ -1,9 +1,9 @@
-import { apothem,inscribed, } from './apothem';
+import { apothem, } from './apothem';
 import { baseAngle, range, } from './operations';
-import { numSides, setX, setY, } from './data';
+import { numSides, rotation, setRotation,setX, setY, } from './data';
 export const tessVector = poly => apothem(poly).mult(2);
 
 export const nthTess = poly => (n = 0) => tessVector(poly).rotate(n * baseAngle(poly));
 export const tessVex = poly => range(numSides(poly)).map(nthTess(poly));
-export const tesselate = poly => tessVex(poly).map(v =>
-[ setX(v.x),setY(v.y), ].reduce((p,fn) => fn(p), poly));
+export const tesselate = poly => tessVex(poly).map((v,i) =>
+[ setX(v.x),setY(v.y), setRotation(rotation(poly) + (baseAngle(poly) * i)), ].reduce((p,fn) => fn(p), poly));
