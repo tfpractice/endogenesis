edaa6f9a8a15d1766553f9f2c263666554a3f86d
theory friction practice <jackson.brebnor@gmail.com>
2017-06-20 11:28:50 -0400

    uses rotate rather than arithmetic for angle calculations also applies center to apothem

--- src/polygon/apothem.js
+++ src/polygon/apothem.js
@@ -1,18 +1,16 @@
 import { Vector, } from 'p5';
 import { numSides, radius, rotation, setRadius, setRotation, } from './data';
 import { baseAngle, } from './operations';
+import { addCenter, normal, } from './vector';
 
-// import { getX, getY, numSides, polygon, radius, rotation,
-//   segments, setNumSides, setRadius, setRotation, setSegments, setX,setY, }
 const { cos, PI, } = Math;
 
 export const apoFactor = poly => cos(PI / numSides(poly));
 export const apoOffset = poly => baseAngle(poly) / 2;
-export const apoDir = poly => rotation(poly) + apoOffset(poly);
+export const apoDir = poly => normal(poly).rotate(apoOffset(poly)).heading();
 export const apoMag = poly => radius(poly) * apoFactor(poly);
 export const apoNorm = poly => Vector.fromAngle(apoDir(poly));
-export const apothem = poly => apoNorm(poly).mult(apoMag(poly));
+export const apothem = poly => addCenter(poly)(apoNorm(poly).mult(apoMag(poly)));
 export const inscribed = poly => 
-[ setRadius(apoMag(poly)), setRotation(apoDir(poly)), ].reduce((p, fn) => fn(p));
-
-// (getX(poly), getY(poly))
+[ setRadius(apoMag(poly)), setRotation(apoDir(poly)), ]
+  .reduce((p, fn) => fn(p));
